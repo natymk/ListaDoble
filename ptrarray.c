121c7de2f5a0a77811dad9e025f89f9bdd1e591d
Nathalia Cambronero <84359719+natymk@users.noreply.github.com>
2021-06-11 15:21:23 -0600

    Update ptrarray.c

--- ptrarray.c
+++ ptrarray.c
@@ -1,1 +1,27 @@
-#include
+#include<stdio.h>
+#include<stdlib.h>
+
+//int *ptr1=NULL;
+//int *ptr2=NULL;
+//int *ptr3=NULL;
+//int *ptr4=NULL;
+
+void main(){
+
+int *ptr1 = (int *)malloc(sizeof(int));
+int *ptr2 = (int *)malloc(sizeof(int));
+int *ptr3 = (int *)malloc(sizeof(int));
+int *ptr4 = (int *)malloc(sizeof(int));
+
+*ptr1=15;
+*ptr2=64;
+*ptr3=70;
+*ptr4=30;
+
+//int *ptrarray[]=NULL;
+//ptrarray[1]=ptr1;
+
+//printf("array[1]: %d\n", ptrarray[1]);
+
+}
+
